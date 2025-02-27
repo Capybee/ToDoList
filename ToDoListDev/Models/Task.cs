987cb43a688eb3022a598dc6f561ac3d0238f3f2
Nikita Vlasov <Mr.NekPro@yandex.ru>
2025-02-28 00:52:31 +0400

    Произвел реверс-инжиниринг БД

---
+++ ToDoListDev/Models/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoListDev.Models;
+
+public partial class Task
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string CreateDate { get; set; } = null!;
+
+    public string CompletionDate { get; set; } = null!;
+
+    public int IsFinal { get; set; }
+}
